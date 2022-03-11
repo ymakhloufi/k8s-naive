173f69cd52a32fd09a13803a32f473c7ae16a4ab
mseld <mahmoud.samy88@gmail.com>
2022-03-11 15:59:09 +0100

    server

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"strconv"
 	"time"
 )
 
-func main() {
+func printstdout() {
 	println("Hello World!")
 	for i := 1; i <= 10; i++ {
 		time.Sleep(time.Second * 1)
 		println("I'm alive for " + strconv.Itoa(i) + " seconds...")
 	}
 	println("Good-Bye World!")
+}
+
+func main() {
+	go printstdout()
+
+	http.HandleFunc("/health", healthy)
+	http.HandleFunc("/ping", pong)
+	http.ListenAndServe(":8090", nil)
+}
+
+func healthy(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "healthy")
+}
+
+func pong(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "pong")
 }
